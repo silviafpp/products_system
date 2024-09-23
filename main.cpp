34d6168d4c9514809ec836580baff13c2482e773
Silvia Pacheco <124101397+silviafpp@users.noreply.github.com>
2024-09-23 23:17:56 +0000

    updates

--- main.cpp
+++ main.cpp
@@ -33,15 +33,55 @@ void adicionarProdutos(Produto produtos[], int& quantidadeAtual) {
 }
 
 void exibirProdutos(const Produto produtos[], int quantidadeAtual) {
-    for(int i = 0; i < 100; i++)
-        cout << produtos[i];
+    for(int i = 0; i < quantidadeAtual; i++) {  
+        cout << "Produto " << i + 1 << " :" << endl;
+        cout << "Nome: " << produtos[i].nome << endl;
+        cout << "Preço: " << produtos[i].preco << endl;
+        cout << "Quantidade: " << produtos[i].quantidade << endl;
+        cout << "-----------------------" << endl;
+    }
+}
+
+float calcularValorTotal(const Produto produtos[], int quantidadeAtual) {
+    float somaTotal = 0;
+    
+    for (int i = 0; i < quantidadeAtual; i++) {
+        somaTotal += produtos[i].preco * produtos[i].quantidade;
+    }
+    return somaTotal;
 }
 
 int main() {
     int quantidadeAtual = 0;
+    int opcao;
 
-    adicionarProdutos(produtos, quantidadeAtual);
-    exibirProdutos(produtos, quantidadeAtual);
+    do {
+        cout << "1. Adicionar Produto" << endl;
+        cout << "2. Exibir Produtos" << endl;
+        cout << "3. Calcular Valor Total" << endl;
+        cout << "4. Sair" << endl;
+        cout << "Escolha uma opção: ";
+        cin >> opcao;
+
+        switch (opcao) {
+            case 1:
+                adicionarProdutos(produtos, quantidadeAtual);
+                break;
+            case 2:
+                exibirProdutos(produtos, quantidadeAtual);
+                break;
+            case 3: {
+                float valorTotal = calcularValorTotal(produtos, quantidadeAtual);
+                cout << "Valor total dos produtos: " << valorTotal << "€" << endl;
+                break;
+            }
+            case 4:
+                cout << "Saindo do programa." << endl;
+                break;
+            default:
+                cout << "Opção inválida. Tente novamente." << endl;
+        }
+    } while (opcao != 4);
 
     return 0;
 }
